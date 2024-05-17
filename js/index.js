70cddc40c4d10929c3d14756c80cd578243e5afa
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-05-17 13:38:50 +0500

    The are being writing codes

---
+++ js/index.js
@@ -0,0 +1,92 @@
+const musicContainerEl = document.getElementById("music-container");
+const albumCoverImageEl = document.getElementById("album-cover-image");
+const progressEl = document.getElementById("progress");
+const progressContainerEl = document.getElementById("progres-container");
+const titleEl = document.getElementById("title");
+const backwardBtn = document.getElementById("backward");
+const playBtn = document.getElementById("play");
+const forwardBtn = document.getElementById("forward");
+const audioEl = document.getElementById("audio")
+
+
+const tracks = [
+    "Dili-me-Jigarlar",
+    "Dili-me-Onajon",
+    "Konsta-Ertasi-yoq",
+];
+
+let currentSong = 0;
+
+const changeMusic = (song) => {
+    titleEl.textContent = `${song}`;
+    audioEl.src = `../audio/${song}.mp3`;
+    albumCoverImageEl.src = `../images/${song}.jpg`;
+};
+
+
+changeMusic(tracks[currentSong]);
+
+const play = () => {
+    musicContainerEl.classList.add("play");
+    document.querySelector(
+        ".btn-big"
+    ).innerHTML = `<i class="fa-solid fa-pause"></i>`
+    audioEl.play();
+}
+
+
+const pause = () => {
+    musicContainerEl.classList.remove("play");
+    document.querySelector(
+        ".btn-big"
+    ).innerHTML = `<i class="fa-solid fa-play"></i>`
+    audioEl.pause();
+}
+
+const playSong = () => {
+    if (musicContainerEl.classList.contains("play")) {
+        pause();
+    } else {
+        play ();
+    }
+};
+
+const nextSong = () => {
+    if (tracks.length-1 > currentSong) {
+        currentSong += 1
+    } else {
+        currentSong = 0
+    }
+    changeMusic(tracks[currentSong]);
+    play()
+}
+
+
+const prevSong = () => {
+    if (currentSong <= 0) {
+        currentSong = tracks.length - 1
+    } else {
+        currentSong -= 1
+    }
+    changeMusic(tracks[currentSong]);
+    play()
+}
+
+const progress = (e) => {
+    const {duration, currentTime} = e.target;
+    const widthTime = (currentTime * 100) / duration;
+    console.log(`${widthTime}%`);
+    progressContainerEl.style.width = `${widthTime}%`;
+};
+
+const progressChange = (e) => {
+    const {duration} = audioEl;
+    const clientX = e.clientX;
+    const progressWidth = e.target
+}
+
+playBtn.addEventListener("click", playSong);
+forwardBtn.addEventListener("click", nextSong); 
+backwardBtn.addEventListener("click", prevSong); 
+audioEl.addEventListener('timeupdate', progress);
+progressEl.addEventListener("click", progressChange);
